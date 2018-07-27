62b833e75974b6e6bbc9f95126a82138d4fa1128
Charles Marks <cmarks@sdsu.edu>
2018-07-27 13:02:49 -0700

    Initial

---
+++ R/connect_to_database.R
@@ -0,0 +1,23 @@
+# This file will handle connecting to the database
+#
+#' Connects to the database and returns a database connection object
+#' @param username Username being used to access database
+#' @param password Password beig used to access database
+#' @return Database connection object
+#' @examples
+#' connect_to_database("MyUserName","MyPassword")
+#
+connect_to_database <- function(username, password) {
+
+  # Load the database driver
+  drv <- dbDriver("PostgreSQL")
+
+  # Load the database connection
+  # If connection established, then will return object representing the connection
+  # If connection not established, then...
+  con <- dbConnect(drv,
+                   host = "130.191.118.107", port = 5432,
+                   user = username, password = password)
+
+  return(con)
+}
