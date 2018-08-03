16c78f53879d80d16683b8c0263cca93407d7af8
Charles Marks <cmarks@sdsu.edu>
2018-08-03 10:40:10 -0700

    working on fetch_das1_trip_summary

--- R/fetch_das1_trip_summary.R
+++ R/fetch_das1_trip_summary.R
@@ -8,7 +8,29 @@
 #' connect_to_database("MyUserName","MyPassword")
 #
 
-fetch_das1_trip_summary <- function(con, deviceID)
+fetch_das1_trip_summary <- function(con, deviceID, startdate)
 {
+   queryStringApr <- "SELECT * FROM spmd.ex_tripsummary_datawsu_april";
+   counter <- 0;
+   if(!missing(deviceID)||!missing(startdate))
+   {
+     queryStringApr <- paste(queryStringApr, "WHERE");
+     if(!missing(deviceID)) {
+       queryStringApr <- paste(queryStringApr, "deviceid =", deviceID);
+       counter <- 1;
+     }
+     if(!missing(startdate)) {
+       if(counter == 1)
+       {
+         queryString <- paste(queryString, "AND")
+       }
+       startstring <- paste("'", startdate, "'", sep="")
+       queryString <- paste(queryString, "startdate =", startstring);
+       counter <- 1;
+     }
+   }
 
+   return_df <- dbGetQuery(con, queryString)
+
+   return(return_df);
 }
