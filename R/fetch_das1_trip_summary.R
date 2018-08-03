4acc664446000c64fe03a1e9df210793f7620f60
Charles Marks <cmarks@sdsu.edu>
2018-08-03 11:16:32 -0700

    fetch_das1_trip_summary is operational, it pulls all entries from the two trip summary tables and allows the user to select entries both by date and by deviceid

--- R/fetch_das1_trip_summary.R
+++ R/fetch_das1_trip_summary.R
@@ -2,35 +2,44 @@
 #
 #'
 #' @param con PostgreSQL db connection object
-#' @param tba
+#' @param deviceID optional search parameter, Device ID
+#' @param startdate optional search parameter, trip date, format 'YYYY-MM-DD'
 #' @return Dataframe containing Das1_Trip_Summary table entries
 #' @examples
-#' connect_to_database("MyUserName","MyPassword")
+#' fetch_das1_trip_summary(con, deviceID = 10107, startdate = '2012-10-01')
 #
 
 fetch_das1_trip_summary <- function(con, deviceID, startdate)
 {
-   queryStringApr <- "SELECT * FROM spmd.ex_tripsummary_datawsu_april";
-   counter <- 0;
-   if(!missing(deviceID)||!missing(startdate))
-   {
-     queryStringApr <- paste(queryStringApr, "WHERE");
-     if(!missing(deviceID)) {
-       queryStringApr <- paste(queryStringApr, "deviceid =", deviceID);
-       counter <- 1;
-     }
-     if(!missing(startdate)) {
-       if(counter == 1)
-       {
-         queryString <- paste(queryString, "AND")
-       }
-       startstring <- paste("'", startdate, "'", sep="")
-       queryString <- paste(queryString, "startdate =", startstring);
-       counter <- 1;
-     }
-   }
+  queryString1 <- "SELECT * FROM spmd.ex_tripsummary_datawsu_april";
+  queryString2 <- "SELECT * FROM spmd.ex_tripsummary_datawsu_october";
+  counter <- 0;
+  if(!missing(deviceID)||!missing(startdate))
+  {
+    queryString1 <- paste(queryString1, "WHERE");
+    queryString2 <- paste(queryString2, "WHERE");
+    if(!missing(deviceID)) {
+      queryString1 <- paste(queryString1, "deviceid =", deviceID);
+      queryString2 <- paste(queryString2, "deviceid =", deviceID);
+      counter <- 1;
+    }
+    if(!missing(startdate)) {
+      if(counter == 1)
+      {
+        queryString1 <- paste(queryString1, "AND");
+        queryString2 <- paste(queryString2, "AND");
+      }
+      startstring <- paste("'", startdate, "'", sep="")
+      queryString1 <- paste(queryString1, "startdate =", startstring);
+      queryString2 <- paste(queryString2, "startdate =", startstring);
+      counter <- 1;
+    }
+  }
+  #queryString1 <- paste(queryString1, "LIMIT 3000;");
+  #queryString2 <- paste(queryString2, "LIMIT 1000;");
+  df1 <- dbGetQuery(con, queryString1);
+  df2 <- dbGetQuery(con, queryString2);
 
-   return_df <- dbGetQuery(con, queryString)
-
-   return(return_df);
+  return_df <- rbind(df1,df2);
+  return(return_df);
 }
