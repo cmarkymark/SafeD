e257782c38029e31cd6c226880b8377ef00b5bf6
Charles Marks <cmarks@sdsu.edu>
2018-08-03 09:54:17 -0700

    created fetch_das1_trip_summary file, non-functional

---
+++ R/fetch_das1_trip_summary.R
@@ -0,0 +1,14 @@
+# This function will pull entries from the Das1_Trip_Summary tablw
+#
+#'
+#' @param con PostgreSQL db connection object
+#' @param tba
+#' @return Dataframe containing Das1_Trip_Summary table entries
+#' @examples
+#' connect_to_database("MyUserName","MyPassword")
+#
+
+fetch_das1_trip_summary <- function(con, deviceID)
+{
+
+}
